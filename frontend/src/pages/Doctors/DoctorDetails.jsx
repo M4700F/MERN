f5d009f840afdd227e36e232e368d77b06dea51e
Maroof Ahmed <96487710+M4700F@users.noreply.github.com>
2024-07-16 18:40:48 +0600

    npm run dev

---
+++ frontend/src/pages/Doctors/DoctorDetails.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DoctorDetails = () => {
+    return (
+        <div>
+            Doctor
+        </div>
+    );
+};
+
+export default DoctorDetails;
---
+++ frontend/src/routes/Routers.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Home from "../pages/Home";
+import Services from "../pages/Services";
+import Signup from "../pages/Signup";
+import Login from "../pages/Login";
+import Doctors from "../pages/Doctors/Doctors";
+import DoctorDetails from "../pages/Doctors/DoctorDetails";
+import Contact from "../pages/Contact";
+
+import { Routes, Route } from "react-router-dom";
+const Routers = () => {
+    return (
+        <div>
+            Routers
+        </div>
+    );
+};
+
+export default Routers;
